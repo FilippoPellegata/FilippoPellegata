fad0f40a44e3083e569ae1399506f4924bc222e0
FilippoPellegata <filippopellegata@gmail.com>
2021-04-21 21:47:50 +0200

    p

--- docs/Offerta Telefonica/calcoli.js
+++ docs/Offerta Telefonica/calcoli.js
@@ -3,19 +3,19 @@ function inizializza() {
     var sesso = [" ", "Maschio", "Femmina","preferisco non definire"];
     var x = document.getElementById("sesso");
     for (var i = 0; i < sesso.length; i++) {
-        s.add(new Option(sesso[i]));
+        x.add(new Option(sesso[i]));
     }
 
     var color = [" ","Verdi","Azzurri", "Marroni"];
     x= document.getElementById("coloreocchi");
     for (var i = 0; i < color.length; i++) {
-        a.add(new Option(color[i]));
+        x.add(new Option(color[i]));
     }
 
     var provincia = [" ","Como", "Milano", "Brescia", "Torino", "Genova"];
     x = document.getElementById("provincia");
     for(var i=0;i<provincia.length;i++){
-        a.add(new Option(provincia[i]));
+        x.add(new Option(provincia[i]));
     }
 }
 
