98d580f6e1e2ad3c8cfef8a2278dc95d49078c1c
FilippoPellegata <filippopellegata@gmail.com>
2021-04-21 22:24:38 +0200

    c

---
+++ docs/quiz/calcoli.js
@@ -0,0 +1,30 @@
+function elabora()
+{
+    var cognome=document.getElementById("cognome").value;
+    var nome=document.getElementById("nome").value;
+    var giorno=parseInt(document.getElementById("giorno").value);
+    var mese=document.getElementById("mese").value;
+    var anno=parseInt(document.getElementById("anno").value);
+    var punteggio=0;
+    if(!controllaCampi(cognome,nome,giorno,mese,anno))
+    {
+        //controllo le risposte alle domande
+        if(document.getElementById("risposta1").value=="SQL")
+            punteggio++;
+        if(document.getElementById("risposta2").value=="2")
+            punteggio++;
+        if(document.getElementById("r1").checked)
+            punteggio++;
+        if(document.getElementById("c1").checked)
+            punteggio++;
+        if(document.getElementById("c3").checked)
+            punteggio++;
+        if(document.getElementById("risposta5").value=="1 byte")
+            punteggio++;
+        //visualizzo la somma dei punti
+        document.getElementById("punteggio").innerHTML=punteggio.toString();
+    }
+    else
+        alert("Campi non inseriti o inseriti non correttamente!");
+
+}
