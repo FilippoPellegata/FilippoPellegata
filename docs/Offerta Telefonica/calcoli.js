b169236d1d5530c1c895d45e0624aa824bbdfa78
FilippoPellegata <filippopellegata@gmail.com>
2021-04-21 21:45:42 +0200

    p

--- docs/Offerta Telefonica/calcoli.js
+++ docs/Offerta Telefonica/calcoli.js
@@ -1,6 +1,24 @@
-function attivaOfferta() {
+function inizializza() {
 
-    alert("offerta selezionata");
+    var sesso = [" ", "Maschio", "Femmina","preferisco non definire"];
+    var x = document.getElementById("sesso");
+    for (var i = 0; i < sesso.length; i++) {
+        s.add(new Option(sesso[i]));
+    }
 
+    var color = [" ","Verdi","Azzurri", "Marroni"];
+    x= document.getElementById("coloreocchi");
+    for (var i = 0; i < color.length; i++) {
+        a.add(new Option(color[i]));
+    }
 
+    var provincia = [" ","Como", "Milano", "Brescia", "Torino", "Genova"];
+    x = document.getElementById("provincia");
+    for(var i=0;i<provincia.length;i++){
+        a.add(new Option(provincia[i]));
+    }
+}
+
+function attivaOfferta(){
+    alert("offerta comprata");
 }
