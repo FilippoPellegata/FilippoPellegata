cdfaab5a57c309a6eff1a2b5a42f9bd7b6e4daa9
FilippoPellegata <filippopellegata@gmail.com>
2021-05-14 23:47:01 +0200

    push

--- docs/eseTreno/calcoli.js
+++ docs/eseTreno/calcoli.js
@@ -5,36 +5,30 @@ function Compra() {
 
 
 
-    if(form.c1.checked){
-      classe = "Prima classe";
-      costoBiglietto = 0.43;
+    if (form.c1.checked) {
+        classe = "Prima classe";
+        costoBiglietto = 0.43;
+    } else if (form.c2.checked) {
+        classe = "Seconda classe"
+        costoBiglietto = 0.20;
     }
-    else if(form.c2.checked){
-      classe = "Seconda classe"
-      costoBiglietto = 0.20;
-    }
-    
+
     var numP = document.getElementById("posti").value;
-    if(numP == 1){
-      costoTot = costoBiglietto  *numP;
+    if (numP == 1) {
+        costoTot = costoBiglietto * numP;
+    } else if (numP == 2) {
+        costoTot = costoBiglietto * numP;
+    } else if (numP == 3) {
+        costoTot = costoBiglietto * numP;
+    } else if (numP == 4) {
+        costoTot = costoBiglietto * numP;
+    } else if (numP == 5) {
+        costoTot = costoBiglietto * numP;
+    } else if (numP == 6) {
+        costoTot = costoBiglietto * numP;
     }
-    else if(numP == 2){
-      costoTot = costoBiglietto * numP;
-    }
-    else if(numP == 3){
-      costoTot = costoBiglietto * numP;
-    }
-    else if(numP == 4){
-      costoTot = costoBiglietto * numP;
-    }
-    else if(numP == 5){
-      costoTot = costoBiglietto * numP;
-    }
-    else if(numP == 6){
-      costoTot = costoBiglietto * numP;
-    }
-    
-   
-        document.getElementById("ris").innerHTML = "Costo:" + costoBiglietto + " €";
-    
-  }
+
+
+    document.getElementById("ris").innerHTML = costoTot;
+
+}
